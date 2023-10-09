5e83338aaed05baa7697f4fc6bc93d4431668556
Volodymyr Donets <volodons@gmail.com>
2023-10-09 23:31:52 +0300

    Add preloader

---
+++ src/components/Preloader/Preloader.jsx
@@ -0,0 +1,7 @@
+const Preloader = () => (
+  <div className="inline-block h-24 w-24 animate-spin rounded-full border-8 border-solid border-blue-500 border-r-transparent align-middle">
+    <span className="sr-only">Loading...</span>
+  </div>
+);
+
+export default Preloader;
